f5bdb7b81cf42dc818a40b1ddce185a9fd0c1a07
rjrayat <rjrayat37@gmail.com>
2025-12-23 13:14:12 +0600

    Database Setup

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+import { createApp, h } from 'vue';
+import './bootstrap';
+import { createInertiaApp, router } from '@inertiajs/vue3'
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager:true})
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+	  const app = createApp({ render: () => h(App, props) })
+		app.use(plugin)
+        app.component('EasyDataTable', Vue3EasyDataTable)
+		app.mount(el)
+	},
+  
+})
+
